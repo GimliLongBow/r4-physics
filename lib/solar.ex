36c678c41c786001c9da8bdc6f26c5f0a93089a8
Andrew Anderson <gimlilongbow@gmail.com>
2016-02-08 22:21:53 -0500

    Adding Solar flare tests.

---
+++ lib/solar.ex
@@ -0,0 +1,6 @@
+defmodule Solar do
+
+  def power(%{classification: :X, scale: s}), do: s * 1000
+  def power(%{classification: :M, scale: s}), do: s * 10
+  def power(%{classification: :C, scale: s}), do: s * 1
+end
