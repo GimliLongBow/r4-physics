42cd524a88b9e9c018fdbf966765767c4b14cee8
Andrew Anderson <gimlilongbow@gmail.com>
2016-02-20 22:12:28 -0500

    Refactoring all the things!

--- lib/solar/flares.ex
+++ lib/solar/flares.ex
@@ -1,24 +1,48 @@
-defmodule Solar.Flares do
+defmodule Solar.Flare do
 
-  def power(%{classification: :X, scale: s}), do: s * 1000
-  def power(%{classification: :M, scale: s}), do: s * 10
-  def power(%{classification: :C, scale: s}), do: s * 1
+  defstruct [
+    classification: :M,
+    scale: 0,
+    power: 0,
+    is_deadly: false,
+    date: nil
+  ]
+
+  def load(flares) do
+    for flare <- flares, do:
+      flare
+        |> calculate_power
+        |> calculate_deadliness
+  end
+
+  def calculate_power(flare) do
+    factor = case flare.classification do
+      :X -> 1000
+      :M -> 10
+      :C -> 1
+    end
+    %{flare | power: flare.scale * factor}
+  end
+
+  def calculate_deadliness(flare) do
+    %{flare | is_deadly: flare.power > 1000}
+  end
 
   def no_eva(flares) do
-    Enum.filter flares, fn(flare) -> power(flare) > 1000 end
+    Enum.filter flares, fn(flare) -> calculate_power(flare).power > 1000 end
   end
 
   def deadliest(flares) do
-    Enum.map(flares, &(power(&1)))
+    Enum.map(flares, &(calculate_power(&1).power))
       |> Enum.max
   end
 
   def total_flare_power(flares) do
-    Enum.map(flares, &(power(&1)))
+    Enum.map(flares, &(calculate_power(&1).power))
       |> Enum.sum
   end
 
   def flare_list(flares) do
-    for flare <- flares, flare.classification === :X, do: %{power: power(flare), is_deadly: power(flare) > 1000}
+    for flare <- flares, flare.classification === :X, do: %{calculate_power: calculate_power(flare), is_deadly: calculate_power(flare) > 1000}
   end
 end
--- test/solar_test.exs
+++ test/solar_test.exs
@@ -1,17 +1,18 @@
 defmodule SolarTest do
   use ExUnit.Case
   use Timex
+  import Solar.Flare
 
   setup do
     flares = [
-      %{classification: :X, scale: 99, date: Date.from({1859, 8, 29})},
-      %{classification: :M, scale: 5.8, date: Date.from({2015, 1, 12})},
-      %{classification: :M, scale: 1.2, date: Date.from({2015, 2, 9})},
-      %{classification: :C, scale: 3.2, date: Date.from({2015, 4, 18})},
-      %{classification: :M, scale: 83.6, date: Date.from({2015, 6, 23})},
-      %{classification: :C, scale: 2.5, date: Date.from({2015, 7, 4})},
-      %{classification: :X, scale: 72, date: Date.from({2012, 7, 23})},
-      %{classification: :X, scale: 45, date: Date.from({2003, 11, 4})},
+      %Solar.Flare{classification: :X, scale: 99, date: Date.from({1859, 8, 29})},
+      %Solar.Flare{classification: :M, scale: 5.8, date: Date.from({2015, 1, 12})},
+      %Solar.Flare{classification: :M, scale: 1.2, date: Date.from({2015, 2, 9})},
+      %Solar.Flare{classification: :C, scale: 3.2, date: Date.from({2015, 4, 18})},
+      %Solar.Flare{classification: :M, scale: 83.6, date: Date.from({2015, 6, 23})},
+      %Solar.Flare{classification: :C, scale: 2.5, date: Date.from({2015, 7, 4})},
+      %Solar.Flare{classification: :X, scale: 72, date: Date.from({2012, 7, 23})},
+      %Solar.Flare{classification: :X, scale: 45, date: Date.from({2003, 11, 4})},
     ]
     {:ok, data: flares}
   end
@@ -20,37 +21,46 @@ defmodule SolarTest do
     assert length(flares) == 8
   end
 
-  test "power for an X class flare is 1000", %{data: flares} do
-    assert Solar.Flares.power(hd(flares)) == 99000
+  test "calculate_power for an X class flare is 1000", %{data: flares} do
+    assert Map.get(calculate_power(hd(flares)), :power) == 99000
   end
 
-  test "power for an M class flare is 10", %{data: flares} do
-    assert Solar.Flares.power(Enum.at(flares, 1)) == 58
+  test "calculate_power for an M class flare is 10", %{data: flares} do
+    assert calculate_power(Enum.at(flares, 1)).power == 58
   end
 
-  test "power for a C class flare is 1", %{data: flares} do
-    assert Solar.Flares.power(Enum.at(flares, 3)) == 3.2
+  test "calculate_power for a C class flare is 1", %{data: flares} do
+    assert calculate_power(Enum.at(flares, 3)).power == 3.2
   end
 
-  test "no_eva returns only flares with a power < 1000", %{data: flares}  do
-    Enum.each Solar.Flares.no_eva(flares), &(assert(Solar.Flares.power(&1)) < 1000)
+  test "no_eva returns only flares with a calculate_power < 1000", %{data: flares}  do
+    Enum.each no_eva(flares), &(assert(calculate_power(&1)) < 1000)
   end
 
   test "no_eva returns three flares < 1000", %{data: flares} do
-    assert length(Solar.Flares.no_eva(flares)) == 3
+    assert length(no_eva(flares)) == 3
   end
 
   test "deadliest flare", %{data: flares} do
-    assert Solar.Flares.deadliest(flares) == 99000
+    assert deadliest(flares) == 99000
   end
 
-  test "total flare power", %{data: flares} do
-    assert Solar.Flares.total_flare_power(flares) == 216911.7
+  test "total flare calculate_power", %{data: flares} do
+    assert total_flare_power(flares) == 216911.7
   end
 
   test "deadly flares", %{data: flares} do
-    #IO.puts Solar.Flares.flare_list(flares)
-    Enum.each Solar.Flares.flare_list(flares), &( if (&1[:power] > 1000), do: assert &1[:is_deadly ] == true)
+    #IO.puts flare_list(flares)
+    Enum.each flare_list(flares), &( if (&1[:calculate_power] > 1000), do: assert &1[:is_deadly ] == true)
+  end
+
+  test "load flares with is_deadly property", %{data: flares} do
+    assert Map.get(hd(load(flares)), :is_deadly) == true
+  end
+
+  test "load flares with calculate_power property", %{data: flares} do
+    flare = hd(load(flares))
+    assert flare.power == 99000
   end
 
 end
